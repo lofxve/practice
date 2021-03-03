a72f7dc1e48966c651733854e27ef3affdbbda62
lofxve <875567313@qq.com>
2021-03-03 13:26:35 +0800

    剑指 Offer 10- II. 青蛙跳台阶问题

---
+++ java-practice/leetcode/src/main/java/com/lofxve/sword/finger/offer/ClimbStairs.java
@@ -0,0 +1,28 @@
+package com.lofxve.sword.finger.offer;
+
+/**
+ * 70. 爬楼梯
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 注意：给定 n 是一个正整数。
+ *
+ * 剑指 Offer 10- II. 青蛙跳台阶问题
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ *
+ * @ClassName ClimbStairs
+ * @Author lofxve
+ * @Date 2021/3/3 13:17
+ * @Version 1.0
+ */
+public class ClimbStairs {
+    public int climbStairs(int n) {
+        int p = 0, q = 0, r = 1;
+        for (int i = 1; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+    }
+}
